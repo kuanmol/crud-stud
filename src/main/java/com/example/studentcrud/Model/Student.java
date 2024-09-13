e4be1df6046ddff046c8834d091c715aff8dbae7
kuanmol <ramkuma100@gmail.com>
2024-09-13 17:29:15 +0530

    Your commit message here

--- src/main/java/com/example/studentcrud/Model/Student.java
+++ src/main/java/com/example/studentcrud/Model/Student.java
@@ -1,11 +1,9 @@
 package com.example.studentcrud.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Student {
@@ -19,7 +17,6 @@ public class Student {
     private String email;
 
     // Getters and Setters
-
     public Long getId() {
         return id;
     }
